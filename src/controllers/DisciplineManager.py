35a8f74288c85c6729e719d9e1c66041194e7739
jrdimande <timoteojrdimande@gmail.com>
2025-08-05 06:47:20 +0200

    style(main): implement PEP8

--- src/controllers/DisciplineManager.py
+++ src/controllers/DisciplineManager.py
@@ -1,12 +1,14 @@
 from src.models.Discipline import Discipline
+
+
 class DisciplineManager:
     def __init__(self):
         self.disciplines = []
 
     # Função para adicionar cadeiras
     def add_discipline(self, name, max_points):
-        if (max_points < 0):
-            max_points = (max_points) * -1
+        if max_points < 0:
+            max_points = max_points * -1
         discipline = Discipline(name, max_points)
         self.disciplines.append(discipline)
 
@@ -20,5 +22,3 @@ class DisciplineManager:
     def ls_disciplines(self):
         for discipline in self.disciplines:
             print(f"{discipline.name.title()}")
-
-
